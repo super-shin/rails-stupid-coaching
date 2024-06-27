c32f27e167d2ca979126f6f7b9fc3649a9858033
Shingie Taira <tairashingie@gmail.com>
2024-06-27 19:15:48 +0900

    added style and graphics

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,23 @@
 class AnswersController < ApplicationController
   def answer
-    @responses = ["Great!", "Silly question, get dressed and go to work", "I don't care, get dressed and go to work!"]
+    @responses = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!", "hmm...", "All good!"]
     @ans = params[:answer_input]
+
+    if @ans.downcase == "i am going to work"
+      @response = @responses[0]
+      @image_url = "/images/c-happy.png"
+    elsif @ans.ends_with?("?")
+      @response = @responses[1]
+      @image_url = "/images/c-angry.png"
+    elsif @ans.include?('think')
+      @response = @responses[3]
+      @image_url = "/images/c-thinking.png"
+    elsif @ans.include?('okay')
+      @response = @responses[4]
+      @image_url = "/images/c-peace.png"
+    else
+      @response = @responses[2]
+      @image_url = "/images/c-disappointed.png"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   # root "posts#index"
   get "/ask", to:"questions#ask"
 
-  get "/answer", to:"answers#answer"
+  post "/answer", to:"answers#answer"
 end
