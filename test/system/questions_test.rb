74a1ef545d67174dbd44b013867f2e97cf1af4a9
Shingie Taira <tairashingie@gmail.com>
2024-06-27 22:01:23 +0900

    added optional test

---
+++ test/system/questions_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "label", text: "Ask your coach anything:"
+    take_screenshot
+  end
+end
